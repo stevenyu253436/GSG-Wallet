466cbb5ec7b531b9a365ce6c297710a47ac3e20f
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-13 12:01:09 +0800

    feat: Implement language-specific text in ServiceFeesView for English and Chinese

--- GSG Wallet/ServiceFeesView.swift
+++ GSG Wallet/ServiceFeesView.swift
@@ -8,31 +8,38 @@
 import SwiftUI
 
 struct ServiceFeesView: View {
+    @AppStorage(selectedLanguageKey) var selectedLanguage: String = "zh-Hant"
+    
     var body: some View {
         VStack {
             Form {
-                Section(header: Text("購買USDT")) {
+                Section(header: Text(languageSpecificText(zhText: "購買USDT", enText: "Buy USDT"))) {
                     Text("1.00%")
                 }
-                Section(header: Text("出售USDT")) {
-                    Text("1.00%(最低 5.00USDT)")
+                Section(header: Text(languageSpecificText(zhText: "出售USDT", enText: "Sell USDT"))) {
+                    Text(languageSpecificText(zhText: "1.00%(最低 5.00USDT)", enText: "1.00% (minimum 5.00 USDT)"))
                 }
-                Section(header: Text("提現到IBAN")) {
-                    Text("€5.00/筆")
+                Section(header: Text(languageSpecificText(zhText: "提現到IBAN", enText: "Withdraw to IBAN"))) {
+                    Text(languageSpecificText(zhText: "€5.00/筆", enText: "€5.00/per"))
                 }
-                Section(header: Text("USDT轉入")) {
-                    Text("免費")
+                Section(header: Text(languageSpecificText(zhText: "USDT轉入", enText: "USDT Transfer In"))) {
+                    Text(languageSpecificText(zhText: "免費", enText: "Free"))
                 }
-                Section(header: Text("USDT轉出(ETH/ERC20)")) {
-                    Text("60.00USDT/筆")
+                Section(header: Text(languageSpecificText(zhText: "USDT轉出(ETH/ERC20)", enText: "USDT Transfer Out (ETH/ERC20)"))) {
+                    Text(languageSpecificText(zhText: "60.00USDT/筆", enText: "60.00 USDT/per"))
                 }
-                Section(header: Text("Unioncash行內轉帳")) {
-                    Text("1.00USDT/筆")
+                Section(header: Text(languageSpecificText(zhText: "GSG Wallet行內轉帳", enText: "GSG Wallet Internal Transfer"))) {
+                    Text(languageSpecificText(zhText: "1.00USDT/筆", enText: "1.00 USDT/transaction"))
                 }
             }
             .listStyle(GroupedListStyle())
         }
-        .navigationTitle("服務費用")
+        .navigationTitle(languageSpecificText(zhText: "服務費用", enText: "Service Fees"))
+    }
+    
+    // Helper method to return the correct language-specific text
+    private func languageSpecificText(zhText: String, enText: String) -> String {
+        return selectedLanguage == "zh-Hant" ? zhText : enText
     }
 }
 
